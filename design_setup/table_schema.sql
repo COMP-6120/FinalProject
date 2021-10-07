531201ac79af897a66b4c9344780b2ff34dc5b7f
Herbert Lin <herbert.lin.7@gmail.com>
2021-10-07 00:28:05 -0500

    Add remaining table schemas

--- design_setup/table_schema.sql
+++ design_setup/table_schema.sql
@@ -26,3 +26,44 @@ CREATE TABLE book (
 	FOREIGN KEY (SubjectID) REFERENCES subject(SubjectID)
 );
 
+CREATE TABLE customer (
+	CustomerID int NOT NULL,
+	LastName varchar(50) NOT NULL,
+	FirstName varchar(50) NOT NULL,
+	Phone char(11) NOT NULL,
+	PRIMARY KEY (CustomerID)
+);
+
+CREATE TABLE employee (
+	EmployeeID int NOT NULL,
+	FirstName varchar(50) NOT NULL,
+	LastName varchar(50) NOT NULL,
+	PRIMARY KEY (EmployeeID)
+);
+
+CREATE TABLE shipper (
+	ShipperID int NOT NULL,
+	ShipperName char(8) NOT NULL,
+	PRIMARY KEY (ShipperID)
+);
+
+CREATE TABLE orders (
+	OrderID int NOT NULL,
+	CustomerID int NOT NULL,
+	EmployeeID int NOT NULL,
+	OrderDate char(8) NOT NULL,
+	ShippedDate char(8) NULL,
+	ShipperID int NULL,
+	PRIMARY KEY (OrderID),
+	FOREIGN KEY (CustomerID) REFERENCES customer(CustomerID),
+	FOREIGN KEY (EmployeeID) REFERENCES employee(EmployeeID),
+	FOREIGN KEY (ShipperID) REFERENCES shipper(ShipperID)
+);
+
+CREATE TABLE order_detail (
+	BookID int NOT NULL,
+	OrderID int NOT NULL,
+	Quantity int NOT NULL,
+	FOREIGN KEY (BookID) REFERENCES book(BookID),
+	FOREIGN KEY (OrderID) REFERENCES orders(OrderID)
+);
