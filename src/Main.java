31f95b9486a916a104245f105cee787697b00937
James <james@wallofhistory.com>
2021-10-12 16:53:24 -0500

    Implement basic Java database connection

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import java.sql.*;
+
+public class Main {
+    static final String DATABASE = "jdbc:mysql://localhost:3307/group10";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "comp6120";
+    static final String QUERY = "SELECT * FROM supplier";
+
+    public static void main(String[] args) {
+        // Open connection.
+        try (Connection conn = DriverManager.getConnection(DATABASE, USERNAME, PASSWORD);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(QUERY)) {
+            // Extract data.
+            while (rs.next()) {
+                // Print by column name.
+                System.out.print("Supplier ID: " + rs.getInt("SupplierID") + "\n");
+                System.out.print("Supplier Name: " + rs.getString("CompanyName") + "\n");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
