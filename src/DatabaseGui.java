9a045f9913bf2ef76f025d25715e6da9d637e747
Joshua Boyd <jaboydfamily@gmail.com>
2021-10-17 14:19:59 -0400

    added JTable component to GUI

--- src/DatabaseGui.java
+++ src/DatabaseGui.java
@@ -6,7 +6,7 @@ public class DatabaseGui extends JFrame {
 	// Instantiate main panel and subpanels
 	private JPanel mainPanel = new JPanel();
 	private JPanel titlePanel = new JPanel();
-	private JPanel tableSelectPanel = new JPanel();
+	private JPanel inputPanel = new JPanel();
 
 	// Fonts
 	private Font titleFont = new Font(Font.SERIF, 
@@ -21,12 +21,29 @@ public class DatabaseGui extends JFrame {
 	// Things that will go on the titlePanel
 	private JLabel appTitle = new JLabel("Database Application");
 	
-	// Things that will go on the tableSelectPanel
 	private String[] dbTables = {"orders", "shipper", "subject",
 								 "book", "customer", "employee",
 								 "supplier", "order_detail"};
 	
 	private JComboBox<String> tableSelect = new JComboBox<>(dbTables);
+
+	// Things that will go on the inputPanel
+	private JTextField inputField = new JTextField(20);
+
+	// Things that will go on the tablePanel
+	private String[] dbTableHeaders = {"Test One", "Test Two", "Test Three",
+									   "Test Four", "Test Five", "Test Six"};
+	
+	private Object[][] dbTableData = {
+		{"One", "Two", "Three", "Four", "Five", "Six"},
+		{"One", "Two", "Three", "Four", "Five", "Six"},
+		{"One", "Two", "Three", "Four", "Five", "Six"},
+		{"One", "Two", "Three", "Four", "Five", "Six"}
+	};
+	private JTable guiTable = new JTable(dbTableData, dbTableHeaders);
+
+	// JScrollPane for the table instead of a JPanel
+	private JScrollPane tablePane = new JScrollPane(guiTable);
 
 
 	DatabaseGui() {
@@ -40,6 +57,7 @@ public class DatabaseGui extends JFrame {
 		// Layout for the panels
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
 		titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.LINE_AXIS));
+		inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.PAGE_AXIS));
 
 		// Adding things to titlePanel
 		appTitle.setFont(titleFont);
@@ -49,8 +67,20 @@ public class DatabaseGui extends JFrame {
 		tableSelect.setFont(componentFont);
 		titlePanel.add(tableSelect);
 
+		// Adding things to inputPanel
+		inputPanel.add(inputField);
+
+		// Adding things to table panel
+		guiTable.setFillsViewportHeight(true);
+
 		// Adding sub panels to main panel
 		mainPanel.add(titlePanel);
+		mainPanel.add(Box.createRigidArea(new Dimension(0, 15)));
+		mainPanel.add(new JSeparator(SwingConstants.HORIZONTAL));
+		mainPanel.add(Box.createRigidArea(new Dimension(0, 15)));
+		mainPanel.add(inputPanel);
+		mainPanel.add(Box.createRigidArea(new Dimension(0, 15)));
+		mainPanel.add(tablePane);
 
 		// Adding main panel to main frame
 		// vertical struct for a bit of space at the top of the window
