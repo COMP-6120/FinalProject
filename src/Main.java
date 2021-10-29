26fa8bd85b653198df5b11f1dc5fc06cf6d7a658
Joshua Boyd <jaboydfamily@gmail.com>
2021-10-28 22:34:40 -0400

    fixed issue with command line args

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,22 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args){
         // GUI Instance
-		DatabaseGui dGui = new DatabaseGui("group10", "3307", "root", "comp6120");
-		dGui.display();
+
+		if(args.length == 0) {
+			DatabaseGui dGui = new DatabaseGui("group10", "3307", "root", "comp6120");
+			dGui.display();
+		} else {
+			
+			// arg[0] = dbName
+			// arg[1] = dbPort
+			// arg[2] = username
+			// arg[3] = password
+			DatabaseGui dGui = new DatabaseGui(args[0],
+											   args[1],
+											   args[2],
+											   args[3]);
+		    dGui.display();
+		}
+		
     }
 }
