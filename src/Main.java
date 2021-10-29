accb1ce84eb72ca643f2d7c3fb9aa89cbc6baf47
Joshua Boyd <jaboydfamily@gmail.com>
2021-10-28 22:31:15 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 import javax.swing.*;
 
-// arg[0] = dbName
-// arg[1] = dbPort
-// arg[2] = user
-// arg[3] = password
 public class Main {
     public static void main(String[] args){
         // GUI Instance
